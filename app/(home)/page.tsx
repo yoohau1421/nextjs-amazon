852921433b8841e2f262ebda1b5aa7d324842cba
Hawyu-Hau <yooctiy142@gmail.com>
2025-08-14 15:43:16 +0700

    3h43- lesson 04

---
+++ app/(home)/page.tsx
@@ -0,0 +1,6 @@
+import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import data from "@/lib/data";
+
+export default async function Page() {
+  return <HomeCarousel items={data.carousels} />;
+}
